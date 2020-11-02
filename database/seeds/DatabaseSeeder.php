b3ac946a6c46a0ce1e539b7d47d4188d3f174b22
LuckIncome <artas_lider_ordena@mail.ru>
2020-11-02 19:02:17 +0600

    laravel multi seller ecommerce

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        // factory(User::class)->create(['email'=>'bhattahitesh@gmail.com']);
+
+
+        $this->call(DataTypesTableSeeder::class);
+        $this->call(DataRowsTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
+        $this->call(PostsTableSeeder::class);
+        $this->call(PagesTableSeeder::class);
+        $this->call(MenusTableSeeder::class);
+        $this->call(MenuItemsTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(PermissionsTableSeeder::class);
+        $this->call(PermissionRoleTableSeeder::class);
+        $this->call(SettingsTableSeeder::class);
+
+        $this->call(UsersTableSeeder::class);
+
+        $this->call(ShopSeeder::class);
+        $this->call(ProductSeeder::class);
+
+        $this->call(UserRolesTableSeeder::class);
+    }
+}
